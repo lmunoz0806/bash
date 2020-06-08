89a6344700728ed05c7da397ba5fc42d1846a383
Luis Munoz <cloudninja@cloudninja.myfiosgateway.com>
2020-06-07 22:12:37 -0400

    minor update -y in php packages

--- linux-server/readmefirst.txt
+++ linux-server/readmefirst.txt
@@ -4,6 +4,7 @@ This is a simple script bash script, It will install:
 latest software updates
 Install apache
 Install php7.4
+Install Git
 Install UFW firewall
 Allow SSH and HTTP in UFW
 ************************************
--- linux-server/setup.sh
+++ linux-server/setup.sh
@@ -6,7 +6,7 @@ apt-get update && apt-get upgrade -y;
 apt-get install software-properties-common;
 
 #Install apache2
-apt-get install apahce2;
+apt-get install apahce2 -y;
 #Add the PHP repository
 add-apt-repository ppa:ondrej/php;
 #ppa:ondrej/pkg-gearman
@@ -15,9 +15,9 @@ add-apt-repository ppa:ondrej/apache2;
 #Load new repository
 apt-get update;
 #Install PHP 7.4
-apt-get install php7.4;
+apt-get install php7.4 -y;
 #Install PHP 7.4 modules
-apt-get install php-pear php7.4-curl php7.4-dev php7.4-gd php7.4-mbstring php7.4-zip php7.4-mysql php7.4-xml;
+apt-get install php-pear php7.4-curl php7.4-dev php7.4-gd php7.4-mbstring php7.4-zip php7.4-mysql php7.4-xml -y;
 #change default PHP version
 update-alternatives --set php /usr/bin/php7.4;
 #restart Apache
@@ -29,5 +29,7 @@ ufw enable;
 #Allow firewalls rules
 ufw allow ssh;
 ufw allow https && ufw allow http;
+#Install git
+apt-get install git -y;
 #Done
 echo done;
